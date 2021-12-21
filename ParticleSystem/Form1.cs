0e41e024a3798ed657aa5d6bb68bbd14d53a0264
КОБА ПИПИЯ <pipiya-2002@mail.ru>
2021-12-22 00:06:20 +0800

    Добавьте файлы проекта.

---
+++ ParticleSystem/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ParticleSystem
+{
+    public partial class Form1 : Form
+    {
+        List<Particle> particles = new List<Particle>();
+        public Form1()
+        {
+            InitializeComponent();
+            picDisplay.Image = new Bitmap(picDisplay.Width, picDisplay.Height);
+
+            for (var i = 0; i < 500; ++i)
+            {
+                var particle = new Particle();
+                // переношу частицы в центр изображения
+                particle.X = picDisplay.Image.Width / 2;
+                particle.Y = picDisplay.Image.Height / 2;
+                // добавляю список
+                particles.Add(particle);
+            }
+        }
+
+        private void UpdateState()
+        {
+            foreach (var particle in particles)
+            {
+                var directionInRadians = particle.Direction / 180 * Math.PI;
+                particle.X += (float)(particle.Speed * Math.Cos(directionInRadians));
+                particle.Y -= (float)(particle.Speed * Math.Sin(directionInRadians));
+            }
+        }
+
+        // функция рендеринга
+        private void Render(Graphics g)
+        {
+            // утащили сюда отрисовку частиц
+            foreach (var particle in particles)
+            {
+                particle.Draw(g);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            UpdateState();
+            using (var g = Graphics.FromImage(picDisplay.Image))
+            {
+                g.Clear(Color.White);
+                Render(g);
+            }
+
+            picDisplay.Invalidate();
+        }
+    }
+}
---
+++ ParticleSystem/Particle.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Drawing;
+
+namespace ParticleSystem
+{
+    class Particle
+    {
+        public int Radius; // радиус частицы
+        public float X; // X координата положения частицы в пространстве
+        public float Y; // Y координата положения частицы в пространстве
+
+        public float Direction; // направление движения
+        public float Speed; // скорость перемещения
+        public float Life;
+        // добавили генератор случайных чисел
+        public static Random rand = new Random();
+
+        // конструктор по умолчанию будет создавать рандомную частицу
+        public Particle()
+        {
+            Direction = rand.Next(360);
+            Speed = 1 + rand.Next(10);
+            Radius = 2 + rand.Next(10);
+            Life = 20 + rand.Next(100);
+        }
+
+        public void Draw(Graphics g)
+        {
+            // создали кисть для рисования
+            var b = new SolidBrush(Color.Black);
+
+            // нарисовали залитый кружок радиусом Radius с центром в X, Y
+            g.FillEllipse(b, X - Radius, Y - Radius, Radius * 2, Radius * 2);
+
+            // удалили кисть из памяти, вообще сборщик мусора рано или поздно это сам сделает
+            // но документация рекомендует делать это самому
+            b.Dispose();
+        }
+    }
+}
